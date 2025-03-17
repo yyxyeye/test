0faeb35b140b6446a2640a3588d81d07569c3f3c
yyxyeye <39752459+yyxyeye@users.noreply.github.com>
2025-03-17 11:03:55 +0800

    Add Kotlin test cases for UserController endpoints

---
+++ src/test/kotlin/com/imooc/controller/UserControllerKotlinTest.kt
@@ -0,0 +1,91 @@
+package com.imooc.controller
+
+import com.imooc.util.CommonUserUtil
+import com.imooc.util.HttpClientUtil
+import com.imooc.util.HostConstant
+import com.imooc.util.URIConstant
+import com.alibaba.fastjson.JSON
+import org.junit.Assert
+import org.junit.Test
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import java.util.HashMap
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class UserControllerKotlinTest {
+
+    @Autowired
+    private lateinit var httpClientUtil: HttpClientUtil
+
+    private var result: Any? = null
+
+    @Test
+    @TestSuitDescription(testCase = "getUser成功，并校验返回值")
+    @Category(NewIntegrationTestCategory::class)
+    fun getUserSuccessTest() {
+        val commonUserUtil = CommonUserUtil()
+        val param: MutableMap<String, String> = HashMap()
+        val response = httpClientUtil.get(
+            HostConstant.PLATFORM_USER_BUSINESS_ADDR + URIConstant.GET_USER,
+            param,
+            commonUserUtil.buildHeader()
+        )
+        result = JSON.parseObject(response)
+        println("result: $result")
+        Assert.assertEquals("返回失败-查询不成功, name不为imooc2", "imooc2", result!!["name"])
+        Assert.assertEquals("返回失败-查询不成功, age不为18", 18, result!!["age"])
+    }
+
+    @Test
+    @TestSuitDescription(testCase = "setUser成功，并校验返回值")
+    @Category(NewIntegrationTestCategory::class)
+    fun setUserSuccessTest() {
+        val commonUserUtil = CommonUserUtil()
+        val param: MutableMap<String, String> = HashMap()
+        val response = httpClientUtil.get(
+            HostConstant.PLATFORM_USER_BUSINESS_ADDR + URIConstant.SET_USER,
+            param,
+            commonUserUtil.buildHeader()
+        )
+        result = JSON.parseObject(response)
+        println("result: $result")
+        Assert.assertEquals("返回失败-设置不成功, name不为imooc2", "imooc2", result!!["name"])
+        Assert.assertEquals("返回失败-设置不成功, age不为18", 18, result!!["age"])
+    }
+
+    @Test
+    @TestSuitDescription(testCase = "getUserJson成功，并校验返回值")
+    @Category(NewIntegrationTestCategory::class)
+    fun getUserJsonSuccessTest() {
+        val commonUserUtil = CommonUserUtil()
+        val param: MutableMap<String, String> = HashMap()
+        val response = httpClientUtil.get(
+            HostConstant.PLATFORM_USER_BUSINESS_ADDR + URIConstant.GET_USER_JSON,
+            param,
+            commonUserUtil.buildHeader()
+        )
+        result = JSON.parseObject(response)
+        println("result: $result")
+        Assert.assertEquals("返回失败-查询不成功, code不为200", 200, result!!["code"])
+        Assert.assertEquals("返回失败-查询不成功, name不为imooc", "imooc", result!!["data"]["name"])
+    }
+
+    @Test
+    @TestSuitDescription(testCase = "hello成功，并校验返回值")
+    @Category(NewIntegrationTestCategory::class)
+    fun helloSuccessTest() {
+        val commonUserUtil = CommonUserUtil()
+        val param: MutableMap<String, String> = HashMap()
+        val response = httpClientUtil.get(
+            HostConstant.PLATFORM_USER_BUSINESS_ADDR + URIConstant.HELLO,
+            param,
+            commonUserUtil.buildHeader()
+        )
+        println("result: $response")
+        Assert.assertEquals("返回失败-查询不成功, 结果不为helloword", "helloword", response)
+    }
+}
